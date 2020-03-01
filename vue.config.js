368e43f715cb26f88c347973408f34cf564b976a
steel <k.kitamura@timers.com>
2020-03-02 00:53:40 +0900

    参照ディレクトリ変更

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   //https://qiita.com/ryusukeeee/items/21f89a9479c468be3534
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/resume/'
-    : '/',
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/resume/'
+  //   : '/',
+  // githubでの公開用にセット
   outputDir: 'docs',
 
   devServer: {
